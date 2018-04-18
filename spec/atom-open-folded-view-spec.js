b32caa329e9ccaa7d47fb05b54de75345ef60d28
Dan Woodson <woodson.dan@gmail.com>
2018-04-18 03:56:06 +0100

    initial commit

---
+++ spec/atom-open-folded-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomOpenFoldedView from '../lib/atom-open-folded-view';
+
+describe('AtomOpenFoldedView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
